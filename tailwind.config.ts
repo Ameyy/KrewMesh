70d190d59f461b3de005e7c89567478fa25a3926
Ameyy Kulkarni <ameykulkarni1993@gmail.com>
2026-09-15 21:43:55 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,12 +13,22 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
         secondary: "var(--secondary)",
         accent: "var(--accent)",
         surface: "var(--surface)",
         'surface-hover': "var(--surface-hover)",
+        "color-1": "hsl(var(--color-1))",
+        "color-2": "hsl(var(--color-2))",
+        "color-3": "hsl(var(--color-3))",
+        "color-4": "hsl(var(--color-4))",
+        "color-5": "hsl(var(--color-5))",
       },
       keyframes: {
+        rainbow: {
+          "0%": { "background-position": "0%" },
+          "100%": { "background-position": "200%" },
+        },
         "marquee-vertical": {
           from: { transform: "translateY(0)" },
           to: { transform: "translateY(calc(-100% - 1rem))" },
@@ -29,6 +39,7 @@ const config: Config = {
         },
       },
       animation: {
+        rainbow: "rainbow var(--speed, 2s) infinite linear",
         "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
         "fade-in-up": "fade-in-up 1s ease-out forwards",
       },
